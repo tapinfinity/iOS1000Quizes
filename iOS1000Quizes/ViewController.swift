fa269a5d86a4524989b829fc95b2940681e2e098
511185591@qq.com <511185591@qq.com>
2018-03-23 15:34:55 +0800

    Initial Commit

---
+++ iOS1000Quizes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS1000Quizes
+//
+//  Created by Jerry on 2018/3/23.
+//  Copyright © 2018年 www.coolketang.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
